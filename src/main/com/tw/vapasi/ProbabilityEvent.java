4335adebf4263601741e6c823c11ee8698cfe0c3
RandeepKaur <randipk@gmail.com>
2019-08-29 13:37:54 +0530

    [Randeep,Madhu]Add. Custom hashcode implementation.

--- src/main/com/tw/vapasi/ProbabilityEvent.java
+++ src/main/com/tw/vapasi/ProbabilityEvent.java
@@ -11,4 +11,13 @@ class ProbabilityEvent {
     boolean equals(ProbabilityEvent probabilityEvent) {
         return (this.probability.compareTo(probabilityEvent.probability) == 0 ? true : false);
     }
+
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + (probability.hashCode());
+        return result;
+    }
 }
