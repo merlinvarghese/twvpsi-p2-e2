d0681d5a21f1ca269a9d196a834db15a3417a26f
RandeepKaur <randipk@gmail.com>
2019-08-29 12:36:24 +0530

    [Randeep,Madhu]Add. Test case for equal probabilities

---
+++ src/main/com/tw/vapasi/ProbabilityEvent.java
@@ -0,0 +1,14 @@
+package com.tw.vapasi;
+
+//Holds the probability of occurrences of an event
+class ProbabilityEvent {
+    private double probability;
+
+    ProbabilityEvent(double probability) {
+        this.probability = probability;
+    }
+
+    boolean isEqual(ProbabilityEvent probability) {
+        return true;
+    }
+}
