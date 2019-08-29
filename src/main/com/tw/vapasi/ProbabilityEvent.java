b50d1ce74c0c8123cb2a76a982e28da5b02bb705
RandeepKaur <randipk@gmail.com>
2019-08-29 15:09:18 +0530

    [Randeep,Madhu]Add. Equals override implementation

--- src/main/com/tw/vapasi/ProbabilityEvent.java
+++ src/main/com/tw/vapasi/ProbabilityEvent.java
@@ -8,12 +8,18 @@ class ProbabilityEvent {
         this.probability = probability;
     }
 
-    boolean equals(ProbabilityEvent probabilityEvent) {
-        return (this.probability.compareTo(probabilityEvent.probability) == 0 ? true : false);
+    @Override
+    public boolean equals(Object obj) {
+        if (this != obj)
+            return false;
+
+        if ((obj == null) || (obj.getClass() != this.getClass()))
+            return false;
+
+        return (this.probability.compareTo(((ProbabilityEvent) obj).probability) == 0);
     }
 
-    public int hashCode()
-    {
+    public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result
