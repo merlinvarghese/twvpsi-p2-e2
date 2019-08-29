9f3cb2b2c400cf5dd244d0c3f5501c200f92b85b
RandeepKaur <randipk@gmail.com>
2019-08-29 16:01:42 +0530

    [Randeep,Madhu]Add. Hashcode test cases and implementation

--- src/main/com/tw/vapasi/ProbabilityEvent.java
+++ src/main/com/tw/vapasi/ProbabilityEvent.java
@@ -1,5 +1,7 @@
 package com.tw.vapasi;
 
+import java.util.Objects;
+
 //Holds the probability of occurrences of an event
 class ProbabilityEvent {
     private Double probability;
@@ -19,11 +21,8 @@ class ProbabilityEvent {
         return (this.probability.compareTo(((ProbabilityEvent) obj).probability) == 0);
     }
 
+    @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result
-                + (probability.hashCode());
-        return result;
+        return Objects.hashCode(this.probability);
     }
 }
